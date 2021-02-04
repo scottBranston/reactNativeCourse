9cd23b2e312e4e20635101c242ba36ccc98e2330
Scott Branston <scott.branston@gmail.com>
2021-02-04 11:33:17 -0800

    Activity Indicator

--- components/DirectoryComponent.js
+++ components/DirectoryComponent.js
@@ -1,8 +1,9 @@
 import React, { Component } from 'react';
-import { FlatList } from 'react-native';
+import { View, FlatList } from 'react-native';
 import { Tile } from 'react-native-elements';
 import { connect } from 'react-redux';
 import { baseUrl } from '../shared/baseUrl';
+import Loading from './LoadingComponent';
 
 const mapStateToProps = state => {
     return {
@@ -15,7 +16,7 @@ class Directory extends Component {
     static navigationOptions = {
         title: 'Directory'
     }
-
+    
     render() {
         const { navigate } = this.props.navigation;
         const renderDirectoryItem = ({item}) => {
@@ -30,6 +31,16 @@ class Directory extends Component {
             );
         };
 
+        if (this.props.campsites.isLoading) {
+            return <Loading />;
+        }
+        if (this.props.campsites.errMess) {
+            return (
+                <View>
+                    <Text>{this.props.campsites.errMess}</Text>
+                </View>
+            );
+        }
         return (
             <FlatList
                 data={this.props.campsites.campsites}
